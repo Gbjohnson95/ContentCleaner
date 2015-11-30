2f2221d4923bb7c3a2bf6544347d3bd0d4437729
Grant Johnson <Gbjohnson95@users.noreply.github.com>
2015-11-30 11:37:43 -0700

    Basic Functionalty
    Changes <b>s to <strong>s, <i>s to <em>s, empty <div>s to <p>s, bolded <span>s to <strong>s, and gets rid of other <span>s.

--- ContentCleaner.js
+++ ContentCleaner.js
@@ -1,8 +1,29 @@
 // ==UserScript==
-// @name         ContentCleaner
-// @namespace    http://tampermonkey.net/
-// @version      0.1
-// @description  Changes Elements In the Page Editing Page
-// @author       Grant Johnson
-// @match        *brightspace.com*
+// @name        ContentCleaner
+// @version     1
+// @author      Grant Johnson
+// @description Flags common mistakes
+// @include     *brightspace.com*
+// @exclude     *brainhoney.com*
+// @require     http://code.jquery.com/jquery-latest.js
+// @run-at      document-end
 // ==/UserScript==
+window.addEventListener("load", function () {
+    ciframe = document.getElementsByTagName('iframe');
+    dctitle = document.querySelector("h1[class*='d2l-page-title']"); // Get the page title.
+    if (dctitle.textContent == "Edit HTML File") { // If the page is editable, run.
+        if (ciframe.length > 0) { // When the frame with the frame with the iframe loads
+            var bs = ciframe[0].contentWindow.document.querySelectorAll("b");
+            $(bs).contents().unwrap().wrap('<strong/>');
+            var is = ciframe[0].contentWindow.document.querySelectorAll("i");
+            $(is).contents().unwrap().wrap('<em/>');
+            var divs = ciframe[0].contentWindow.document.querySelectorAll("div:not([class]):not([id])");
+            $(divs).contents().unwrap().wrap('<p/>');
+            var bolds = ciframe[0].contentWindow.document.querySelectorAll("span[style*='bold']");
+            $(bolds).contents().unwrap().wrap('<strong/>');
+            var spans = ciframe[0].contentWindow.document.querySelectorAll("span:not([style])");
+            $(spans).contents().unwrap();
+        }
+    }
+
+});

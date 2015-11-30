ec06a4e753a7228e417d98c586db98cf9fbfaac8
Grant Johnson <gbjohnson@me.com>
2015-11-30 09:39:49 -0700

    Initial Commit

---
+++ ContentCleaner.js
@@ -0,0 +1,8 @@
+// ==UserScript==
+// @name         ContentCleaner
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Changes Elements In the Page Editing Page
+// @author       Grant Johnson
+// @match        *brightspace.com*
+// ==/UserScript==
